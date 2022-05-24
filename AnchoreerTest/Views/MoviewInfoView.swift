a536acfcabffe0501fb379ab2189ae8a45f93b7e
SophieKim <ksoobean@naver.com>
2022-05-24 22:48:52 +0900

    [main] MovieInfoView 클래스 추가

---
+++ AnchoreerTest/Views/MoviewInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviewInfoView.swift
+//  AnchoreerTest
+//
+//  Created by 김수빈 on 2022/05/24.
+//
+
+import UIKit
+
+class MoviewInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
